93108b23238f21c0198b5c49b5952fc8f9b956ad
Alizagibi <azagibi@gmail.com>
2025-04-20 21:00:59 +0300

    update

--- src/myTest1/Test1.java
+++ src/myTest1/Test1.java
@@ -1,6 +1,5 @@
 package myTest1;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -12,55 +11,47 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Test1 {
-	
- WebDriver driver = new EdgeDriver();
- 
- String THEWEBSITE ="https://www.saucedemo.com/";
- 
- @BeforeTest
- public void mysetup() {
-	 
-	driver.get(THEWEBSITE);
-	 driver.manage().window().maximize();
- }
- 
- 
- @Test(priority=1)
- public void login() throws InterruptedException{
-	 
-	driver.findElement(By.id("user-name")).sendKeys("standard_user");
-	driver.findElement(By.id("password")).sendKeys("secret_sauce");
-	
-	Thread.sleep(2000);
-	driver.findElement(By.id("login-button")).click();
-	
-	
- }
- 
- @Test(priority=2)
- public void addOneRandomItem() throws InterruptedException {
+//this is myfristproject in githup 
+	WebDriver driver = new EdgeDriver();
 
-	 driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+	String THEWEBSITE = "https://www.saucedemo.com/";
+
+	@BeforeTest
+	public void mysetup() {
+
+		driver.get(THEWEBSITE);
+		driver.manage().window().maximize();
+	}
+
+	@Test(priority = 1)
+	public void login() throws InterruptedException {
+
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+		Thread.sleep(2000);
+		driver.findElement(By.id("login-button")).click();
+
+	}
+
+	@Test(priority = 2)
+	public void addOneRandomItem() throws InterruptedException {
+
+		driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
 		Thread.sleep(1000);
-		
-	 
- }
- 
- @Test(priority=3)
- public void removeOneRandomItem() {
-	 driver.findElement(By.id("remove-sauce-labs-backpack")).click();
-	 
- }
- 
- 
- 
- @AfterTest
- public void myAfterTest() {
-//	 driver.quit();
-	 
-	 
- }
-	
 
+	}
+
+	@Test(priority = 3)
+	public void removeOneRandomItem() {
+		driver.findElement(By.id("remove-sauce-labs-backpack")).click();
+
+	}
+
+	@AfterTest
+	public void myAfterTest() {
+//	 driver.quit();
+
+	}
 
 }
